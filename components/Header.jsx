898d1290f2ce186db9963f7a51fbfc18f44c9b90
genildocs <genildocs@gmail.com>
2024-10-31 06:21:04 -0300

    commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ToggleTheme from './ToggleTheme';
+
+export default function Header() {
+  return (
+    <div>
+      <ToggleTheme />
+    </div>
+  );
+}
---
+++ components/ToggleTheme.jsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { Button } from './ui/button';
+export default function ToggleTheme() {
+  return <div>ToggleTheme</div>;
+}
